f4198caa5acc1b5e6f893347461c540df1e1a626
Giulio Cusenza <92781048+GiulioCusenza@users.noreply.github.com>
2024-03-02 02:30:08 +0100

    improve

--- code/App.py
+++ code/App.py
@@ -1,17 +1,28 @@
+import sys, os
+
 from SemReply import SemReply
 
-"""
-(Note to delete)
-
-Interface (on the terminal or GUI) for SemReply
-"""
 
 def app():
+    """
+    Terminal interface for SemReply.
+    """
     prompt = "x"
     while prompt.strip():
-        prompt = input("\nAsk a question about a famous person, event, place, or anything:\n")
-        for i, result in enumerate(SemReply.ask(prompt)):
-            print("\t"+str(i)+".", result)
+        # prompt user
+        question = input("\nAsk a question about a famous person, event, place, or anything:\n")
+        if question.strip() == "":
+            exit(0)
+        
+        # get results
+        sys.stdout = open(os.devnull, 'w') # disable writing to stdout (printing) FIXME
+        results = SemReply.ask(question)
+        sys.stdout = sys.__stdout__ # enable writing to stdout FIXME
+        
+        # print results
+        for i, result in enumerate(results):
+            print("\t"+str(i+1)+".", result.strip())
+
 
 if __name__ == "__main__":
     app()
