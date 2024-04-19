f0152a89118d3e647491e9d228212a9da781f6c4
Giulio Cusenza <92781048+GiulioCusenza@users.noreply.github.com>
2024-04-19 15:31:57 +0200

    time measurements

--- code/evaluate_SBERT.py
+++ code/evaluate_SBERT.py
@@ -15,23 +15,26 @@ embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
 results = dict()
 for FILE in FILES:
+    # Load data
     gold_scores = dict()
     sentences = list()
     lines = open(DIRECTORY+"/"+FILE, "r", encoding="utf-8")
     question = next(lines).strip()
-    print(question)
     for i, line in enumerate(lines):
         score, sentence = line.strip().split(",", 1)
         sentences.append(sentence.strip("\""))
         gold_scores.update({i: int(score)})
 
+    # SBERT ranking
+    start_time = time()
     question_embedding = embedder.encode(question, convert_to_tensor=True)
-    article_embeddings = embedder.encode(sentences, convert_to_tensor=True, show_progress_bar=True)
-    
+    article_embeddings = embedder.encode(sentences, convert_to_tensor=True)
     cos_scores = util.cos_sim(question_embedding, article_embeddings)[0]
     top_results = torch.topk(cos_scores, k=N_SENTENCES)
-    gold = [gold_scores[i.item()] for i in top_results[1]]
+    end_time = time() - start_time
     
+    # Compute NCDG
+    gold = [gold_scores[i.item()] for i in top_results[1]]
     results[FILE] = ndcg_score([gold[:N_SENTENCES]], [top_results[0][:N_SENTENCES]])
     OUTPUT_FILE.write(question+"\n")
-    OUTPUT_FILE.write(str(results[FILE])+"\n\n")
+    OUTPUT_FILE.write(f"NDCG: {results[FILE]} - Time: {end_time} seconds\n\n")
