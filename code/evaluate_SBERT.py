dd6e699b8c40df1b0f8f1c1706c4390f388e74c4
Giulio Cusenza <92781048+GiulioCusenza@users.noreply.github.com>
2024-04-19 15:09:53 +0200

    evaluate SBERT+SemanticSearch

---
+++ code/evaluate_SBERT.py
@@ -0,0 +1,37 @@
+from sklearn.metrics import ndcg_score
+from time import time
+import os
+
+from sentence_transformers import SentenceTransformer, util
+import torch
+
+
+DIRECTORY = "evaluation items"
+FILES = [os.fsdecode(file) for file in os.listdir(DIRECTORY)]
+OUTPUT_FILE = open("evaluation_SBERT_20.txt", "a", encoding="utf-8")
+N_SENTENCES = 20
+
+embedder = SentenceTransformer("all-MiniLM-L6-v2")
+
+results = dict()
+for FILE in FILES:
+    gold_scores = dict()
+    sentences = list()
+    lines = open(DIRECTORY+"/"+FILE, "r", encoding="utf-8")
+    question = next(lines).strip()
+    print(question)
+    for i, line in enumerate(lines):
+        score, sentence = line.strip().split(",", 1)
+        sentences.append(sentence.strip("\""))
+        gold_scores.update({i: int(score)})
+
+    question_embedding = embedder.encode(question, convert_to_tensor=True)
+    article_embeddings = embedder.encode(sentences, convert_to_tensor=True, show_progress_bar=True)
+    
+    cos_scores = util.cos_sim(question_embedding, article_embeddings)[0]
+    top_results = torch.topk(cos_scores, k=N_SENTENCES)
+    gold = [gold_scores[i.item()] for i in top_results[1]]
+    
+    results[FILE] = ndcg_score([gold[:N_SENTENCES]], [top_results[0][:N_SENTENCES]])
+    OUTPUT_FILE.write(question+"\n")
+    OUTPUT_FILE.write(str(results[FILE])+"\n\n")
