36fe85890f3cbaebd0d0828ffb47dfc1e2cd569e
Giulio Cusenza <92781048+GiulioCusenza@users.noreply.github.com>
2024-04-20 21:16:12 +0200

    add log time plot

--- code/plot.py
+++ code/plot.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import csv
+import numpy as np
 
 FILES = {
     "SBERT": "evaluation_SBERT_20.csv",
@@ -29,9 +30,9 @@ fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
 fig.suptitle("Ranking Performance", fontweight="bold")
 fig.text(0.5, 0.04, "Normalized Discounted Cumulative Gain (NDCG)", ha="center")
 fig.text(0.04, 0.5, "N samples", va="center", rotation="vertical")
-ax1.hist(sorted(ndcg["SBERT"]), linewidth=8.0)
+ax1.hist(sorted(ndcg["SBERT"]), color="#4dff4d")
 ax1.set_title("SBERT")
-ax2.hist(sorted(ndcg["Smatch"]), linewidth=8.0)
+ax2.hist(sorted(ndcg["Smatch"]), color="#6699ff")
 ax2.set_title("Smatch")
 plt.show()
 
@@ -41,10 +42,24 @@ print("SBERT Average:", round(sum(time["SBERT"]) / len(time["SBERT"]), 2))
 print("Smatch Average:", round(sum(time["Smatch"]) / len(time["Smatch"]), 2))
 fig, (ax1, ax2) = plt.subplots(2, sharey=True)
 fig.suptitle("Time Performance", fontweight="bold")
-fig.text(0.5, 0.04, "Time (Seconds)", ha="center")
+fig.text(0.5, 0.04, "Time (seconds)", ha="center")
 fig.text(0.04, 0.5, "N samples", va="center", rotation="vertical")
-ax1.hist(sorted(time["SBERT"])[:-1], linewidth=8.0) # remove outlier
+ax1.hist(sorted(time["SBERT"])[:-1], color="#4dff4d") # remove outlier due to initialization of model during timing
 ax1.set_title("SBERT")
-ax2.hist(sorted(time["Smatch"]), linewidth=8.0)
+ax2.hist(sorted(time["Smatch"]), color="#6699ff")
+ax2.set_title("Smatch")
+plt.show()
+
+# Plot log time performance
+print("\nTime performance")
+print("SBERT Average:", round(sum(time["SBERT"]) / len(time["SBERT"]), 2))
+print("Smatch Average:", round(sum(time["Smatch"]) / len(time["Smatch"]), 2))
+fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
+fig.suptitle("Time Performance", fontweight="bold")
+fig.text(0.5, 0.04, "Time (log seconds)", ha="center")
+fig.text(0.04, 0.5, "N samples", va="center", rotation="vertical")
+ax1.hist(sorted(np.log(time["SBERT"]))[:-1], color="#4dff4d") # remove outlier due to initialization of model during timing
+ax1.set_title("SBERT")
+ax2.hist(sorted(np.log(time["Smatch"])), color="#6699ff")
 ax2.set_title("Smatch")
 plt.show()
